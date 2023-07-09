506f36bf488b07a6c8fe13c3cf9f3fe7958f0d24
u_alex90 <u_alex90@mail.ru>
2023-07-09 15:24:43 +1000

    creat pymodbustcp

--- pymodbus/main.py
+++ pymodbus/main.py
@@ -2,7 +2,7 @@ from pymodbus.client import ModbusTcpClient
 
 client = ModbusTcpClient(host='192.168.3.9', port=502)
 client.connect()
-client.write_coil(1, False)
+client.write_coil(1, True)
 result = client.read_coils(4, 1)
 print(result.bits[7])
 client.close()
