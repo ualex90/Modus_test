34473cea1775eee5b2d2b8e071180e91cd854878
u_alex90 <u_alex90@mail.ru>
2023-07-09 15:19:58 +1000

    add requirements.txt

--- main_pymodbus.py
+++ main_pymodbus.py
@@ -1,8 +1,8 @@
 from pymodbus.client import ModbusTcpClient
 
-client = ModbusTcpClient('192.168.3.8')
+client = ModbusTcpClient(host='192.168.3.9', port=502)
 client.connect()
-client.write_coil(1, False, 8)
-result = client.read_coils(1, 1, 8)
+client.write_coil(1, False)
+result = client.read_coils(4, 1)
 print(result.bits[7])
 client.close()
